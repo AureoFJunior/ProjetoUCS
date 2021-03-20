3e28db7c763077dc06deec320f97e2dca3611e0e
AureoFJunior <aureofajardojunior@gmail.com>
2021-03-20 14:11:49 -0300

    UCS Commit 1.0.7

--- src/sample/ControllerPesq.java
+++ src/sample/ControllerPesq.java
@@ -1,19 +1,77 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import lombok.SneakyThrows;
 
-public class ControllerPesq {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ControllerPesq implements Initializable {
 
     @FXML
-    public TextField txtTitle, txtIsbn, txtActor, txtEditora, txtDate;
+    public TextField txtTitle, txtIsbn, txtActor, txtEditora, txtDate, txtDateFinal;
 
     @FXML
     public Button btnSearch;
 
     @FXML
     public ListView listViewPesq;
+
+
+    @SneakyThrows
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        PesqClass auxDbTwo = new PesqClass();
+        Connection connAux = auxDbTwo.conex();
+
+
+        //DataBaseClass auxDel = new DataBaseClass();
+        //Connection con = auxDel.connec();
+
+        //Refresh();
+        if (connAux != null)
+            connAux.close();
+    }
+
+    @FXML
+    public void Refresh() throws SQLException {
+
+        //This method refresh the list view, reading from the database to add the elements.
+
+        if(listViewPesq != null){
+
+            listViewPesq.getItems().clear();
+        }
+
+
+        PesqClass auxDel = new PesqClass();
+        Connection con = auxDel.conex();
+        ObraClass obj = new ObraClass();
+
+
+
+        List<ObraClass> lista = auxDel.pesquisa(obj, txtTitle, txtIsbn, txtActor, txtEditora, txtDate, txtDateFinal);
+        for (ObraClass item: lista) {
+            if (item != null){
+                //listView.getItems().add("Teste");
+                listViewPesq.getItems().add(item.Id + "-" + item.Titulo);
+            }
+
+            else {
+                System.out.println("ERRO KRL");
+            }
+        }
+
+        //listView.setItems(listAux);
+
+
+    }
 
 }
--- src/sample/PesqClass.java
+++ src/sample/PesqClass.java
@@ -3,9 +3,8 @@ package sample;
 import javafx.scene.control.TextField;
 import lombok.Data;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -39,9 +38,9 @@ import java.util.List;
         return conecta;
     }
 
-    public List<PesqClass> pesquisa(ObraClass obra, TextField txtTitle,
+    public List<ObraClass> pesquisa(ObraClass obra, TextField txtTitle,
                          TextField txtIsbn, TextField txtActor,
-                         TextField txtEditora, TextField txtDate) {
+                         TextField txtEditora, TextField txtDate, TextField txtDateFinal) throws SQLException {
 
         PesqClass pesq = new PesqClass();
 
@@ -51,48 +50,84 @@ import java.util.List;
         pesq.Isbn = txtIsbn.getText();
         pesq.Autor = txtActor.getText();
         pesq.Editora = txtEditora.getText();
-        pesq.DataInicial = Integer.parseInt(txtDate.getText());
-        //pesq.DataFinal = Integer.parseInt(txtDateFinal.getText());
+        if (txtDate.getText() != "" && txtDateFinal.getText() != ""){
+            pesq.DataInicial = Integer.parseInt(txtDate.getText());
+            pesq.DataFinal = Integer.parseInt(txtDateFinal.getText());
+        }
+        else {
+            pesq.DataInicial = 0;
+            pesq.DataFinal = 9999;
+        }
+
 
         /*String sqlAux = String.format(" \"%s\", \"%s\", \"%s\", \"%s\", \"%s\"  ",
                 pesq.Title, pesq.Isbn, pesq.Autor, pesq.Editora, pesq.Data);*/
 
         String sql = "SELECT * FROM obras";
 
-        List<String> where = null;
+        List<String> where = new ArrayList<String>() {
+        };
 
-        if(pesq.Title != null){
-            where.add(" obr_nome= '" + pesq.Title + "'");
+        if(pesq.Title != ""){
+            where.add(" obr_nome LIKE %'" + pesq.Title + "'%");
         }
-        if(pesq.Isbn != null){
-            where.add(" obr_isbn= '" + pesq.Isbn + "'");
+        if(pesq.Isbn != ""){
+            where.add(" obr_isbn LIKE %'" + pesq.Isbn + "'%");
         }
-        if(pesq.Editora != null){
-            where.add(" obr_editora= '" + pesq.Editora + "'");
+        if(pesq.Editora != ""){
+            where.add(" obr_editora LIKE %'" + pesq.Editora + "'%");
         }
-        if(pesq.Autor != null){
-            where.add(" obr_autores= '" + pesq.Autor + "'");
+        if(pesq.Autor != ""){
+            where.add(" obr_autores LIKE %'" + pesq.Autor + "'%");
         }
         if(!pesq.DataInicial.equals(null)){
-            where.add(" obr_anopub= " + pesq.DataInicial + "");
+            where.add(" obr_anopub > '" + pesq.DataInicial + "'");
         }
         if(!pesq.DataFinal.equals(null)){
-            where.add(" obr_anopub= " + pesq.Data + "");
+            where.add(" obr_anopub < '" + pesq.DataFinal + "'");
+        }
+
+        if (where.size() > 0)
+        {
+            sql += " WHERE ";
         }
 
 
+        for (int i = 0; i < where.size(); i++)
+        {
+            sql += " " + where.get(i);
+            if (i != where.size() - 1)
+                sql += " AND ";
 
-        PreparedStatement st = conn.prepareStatement(sql);
-        int rows = st.executeUpdate();
+        }
+
+        Statement st = conAux.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        //rs = st.executeQuery(sql);
+        List<ObraClass> listinha = null;
+
+        while (rs.next()) {
+
+            ObraClass item = new ObraClass();
+
+            item.Id = rs.getInt("obr_id");
+            item.Titulo = rs.getString("obr_nome");
+            item.Isbn = rs.getString("obr_isbn");
+            item.Autores = rs.getString("obr_autores");
+            item.Editora = rs.getString("obr_editora");
+            item.Lanc = rs.getInt("obr_anopub");
+
+
+
+            listinha.add(item);
+        }
 
         if (st != null){
             st.close();
-            conn.close();
+            conAux.close();
         }
 
-
-
-
+        return listinha;
 
 
     }
